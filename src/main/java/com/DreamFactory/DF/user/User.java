e107c3aed1b7ddb05c2c9589b72e784506808c1c
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-07-08 14:59:42 +0200

    feat: add basic user model

--- src/main/java/com/DreamFactory/DF/user/User.java
+++ src/main/java/com/DreamFactory/DF/user/User.java
@@ -1,4 +1,30 @@
 package com.DreamFactory.DF.user;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+
 }
