fe0c6f22eab8320f4c0a6e6bc816eb26a9da7b64
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-07-10 13:38:46 +0200

    feat: Add UserService with CRUD and Spring Security integration: - Implement loadUserByUsername for Spring Security authentication. - Encode passwords using PasswordEncoder.

---
+++ src/main/java/com/DreamFactory/DF/user/UserService.java
@@ -0,0 +1,104 @@
+package com.DreamFactory.DF.user;
+
+import com.DreamFactory.DF.user.dto.UserMapper;
+import com.DreamFactory.DF.user.dto.UserRequest;
+import com.DreamFactory.DF.user.dto.UserResponse;
+import com.DreamFactory.DF.user.model.Role;
+import com.DreamFactory.DF.user.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+        if(optionalUser.isEmpty()){
+            throw new UsernameNotFoundException(username + "does not exist.");
+        }
+
+        User user = optionalUser.orElseThrow();
+
+        List<GrantedAuthority> authorities = user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+                .collect(Collectors.toList());
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                true,
+                true,
+                true,
+                true,
+                authorities);
+    }
+
+    public UserResponse registerUser(UserRequest request) {
+        Optional<User> isExistingUsername = userRepository.findByUsername(request.username());
+        if (isExistingUsername.isPresent()){
+            throw new RuntimeException("Username already exist");
+        }
+        Optional<User> isExistingEmail = userRepository.findByEmail(request.username());
+        if (isExistingEmail.isPresent()){
+            throw new RuntimeException("Email already exist");
+        }
+        User user = UserMapper.toEntity(request);
+        user.setPassword(passwordEncoder.encode(request.password()));
+        user.setRoles(Set.of(Role.USER));
+        User savedUser = userRepository.save(user);
+        return UserMapper.fromEntity(savedUser);
+    }
+
+    public List<UserResponse> getAllUsers() {
+        return userRepository.findAll().stream()
+                .map(UserMapper::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    public UserResponse updateUser(Long id, UserRequest request) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Not user with this ID"));
+
+        if (request == null){
+            throw new RuntimeException("The request is not valid");
+        }
+
+        Optional<User> isExistingUsername = userRepository.findByUsername(request.username());
+        if (isExistingUsername.isPresent()){
+            throw new RuntimeException("Username already exist");
+        }
+        Optional<User> isExistingEmail = userRepository.findByEmail(request.username());
+        if (isExistingEmail.isPresent()){
+            throw new RuntimeException("Email already exist");
+        }
+        user.setUsername(request.username());
+        user.setEmail(request.email());;
+        if (request.password() != null && !request.password().isEmpty()) {
+            user.setPassword(passwordEncoder.encode(request.password()));
+        }
+        User updatedUser = userRepository.save(user);
+        return UserMapper.fromEntity(updatedUser);
+    }
+}
