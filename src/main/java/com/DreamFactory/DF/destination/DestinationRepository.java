9568bdf5fa83bbf1640c207ebc743a0687d8d149
tizzifona <tizzifona@gmail.com>
2025-07-10 14:52:02 +0200

    feat: added DestinationRepository extends JpaRepository

---
+++ src/main/java/com/DreamFactory/DF/destination/DestinationRepository.java
@@ -0,0 +1,6 @@
+package com.DreamFactory.DF.destination;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DestinationRepository extends JpaRepository<Destination, Long> {
+}
