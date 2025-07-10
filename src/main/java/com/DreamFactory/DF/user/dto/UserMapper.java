79876ffac6f2d1f6dff5115acbb23046fd2fd66b
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-07-10 13:30:01 +0200

    feat(dto): add UserRequest, UserResponse, UserMapper

---
+++ src/main/java/com/DreamFactory/DF/user/dto/UserMapper.java
@@ -0,0 +1,33 @@
+package com.DreamFactory.DF.user.dto;
+
+import com.DreamFactory.DF.user.model.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public static UserResponse fromEntity(User user){
+        if (user == null){
+            return null;
+        }
+
+        return new UserResponse(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail()
+        );
+    }
+
+    public static User toEntity(UserRequest userRequest){
+        if (userRequest == null){
+            return null;
+        }
+
+        User user = User.builder()
+                .username(userRequest.username())
+                .email(userRequest.email())
+                .build();
+
+        return user;
+    }
+}
+
