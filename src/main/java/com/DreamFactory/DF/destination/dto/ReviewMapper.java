21d30e35d916683bf058af54fdfaefb5d265ddec
NepyAnna <nepyann@gmail.com>
2025-07-11 12:35:30 +0200

    feat: added ReviewMapper

---
+++ src/main/java/com/DreamFactory/DF/destination/dto/ReviewMapper.java
@@ -0,0 +1,22 @@
+package com.DreamFactory.DF.destination.dto;
+
+import com.DreamFactory.DF.review.Review;
+import com.DreamFactory.DF.review.dtos.ReviewRequest;
+import com.DreamFactory.DF.review.dtos.ReviewResponse;
+
+public class ReviewMapper {
+    public static Review toEntity(ReviewRequest request){
+        return Review.builder()
+                .rating(request.rating())
+                .body(request.body())
+                .build();
+    }
+
+    public static ReviewResponse toReviewResponse(Review review){
+        return new ReviewResponse(review.getId(),
+                review.getRating(),
+                review.getBody(),
+                review.getCreatedAt(),
+                review.getUser().getUsername());
+    }
+}
--- src/main/java/com/DreamFactory/DF/review/dtos/ReviewResponse.java
+++ src/main/java/com/DreamFactory/DF/review/dtos/ReviewResponse.java
@@ -1,10 +1,10 @@
 package com.DreamFactory.DF.review.dtos;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 public record ReviewResponse(Long id,
                              double rating,
                              String body,
-                             LocalDateTime createdAt) {
+                             LocalDateTime createdAt,
+                             String username) {
 }
