4f8eda50062ea9641a16813e5481507837c99081
NepyAnna <nepyann@gmail.com>
2025-07-11 11:56:08 +0200

    feat: added Review model

--- src/main/java/com/DreamFactory/DF/review/Review.java
+++ src/main/java/com/DreamFactory/DF/review/Review.java
@@ -1,4 +1,40 @@
 package com.DreamFactory.DF.review;
 
+import com.DreamFactory.DF.destination.Destination;
+import com.DreamFactory.DF.user.model.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    private double rating;
+
+    @NotNull
+    private String body;
+
+    private Date createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "destination_id", nullable = false)
+    private Destination destination;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
---
+++ src/main/java/com/DreamFactory/DF/review/dtos/ReviewRequest.java
@@ -0,0 +1,6 @@
+package com.DreamFactory.DF.review.dtos;
+
+import jakarta.validation.constraints.*;
+
+public record ReviewRequest() {
+}
