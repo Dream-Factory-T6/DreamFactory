e63ec55f8ca30bf0cfc7463e922c9ac3355589fe
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-07-12 14:21:10 +0200

    test: Add user resgistration test in endpoint /register

---
+++ src/test/java/com/DreamFactory/DF/User/UserControllerTest.java
@@ -0,0 +1,48 @@
+package com.DreamFactory.DF.User;
+
+
+import com.DreamFactory.DF.user.dto.UserRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @Transactional
+    void should_registerUser_fromRequest() throws Exception{
+        UserRequest userRequest = new UserRequest("userTest", "usertest@test.com", "password123");
+
+        mockMvc.perform(post("/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.username").value("userTest"))
+                .andExpect(jsonPath("$.email").value("usertest@test.com"))
+                .andExpect(jsonPath("$.id").exists())
+        ;
+    }
+
+}
