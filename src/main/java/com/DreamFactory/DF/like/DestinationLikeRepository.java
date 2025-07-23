ce8b0ab234530d7ea120e25e165a51278395bf20
NepyAnna <nepyann@gmail.com>
2025-07-23 20:39:42 +0200

    feat: added DestinationLikeService implementation

---
+++ src/main/java/com/DreamFactory/DF/like/DestinationLikeRepository.java
@@ -0,0 +1,18 @@
+package com.DreamFactory.DF.like;
+
+import com.DreamFactory.DF.destination.Destination;
+import com.DreamFactory.DF.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DestinationLikeRepository extends JpaRepository<DestinationLike, Long> {
+
+    Optional<DestinationLike> findByUserAndDestination(User user, Destination destination);
+
+    boolean existsByUserAndDestination(User user, Destination destination);
+
+    long countByDestination(Destination destination);
+}
