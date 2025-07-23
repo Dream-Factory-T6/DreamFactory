d3cfb5f68e5a41ad060063a4c7814f571a93db88
NepyAnna <nepyann@gmail.com>
2025-07-23 20:35:37 +0200

    feat: added DestinationLike model,respose and mapper

---
+++ src/main/java/com/DreamFactory/DF/like/DestinationLike.java
@@ -0,0 +1,41 @@
+package com.DreamFactory.DF.like;
+
+import com.DreamFactory.DF.destination.Destination;
+import com.DreamFactory.DF.user.model.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "destination_likes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DestinationLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "destination_id", nullable = false)
+    private Destination destination;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
+
